c7d0cc6fee547cfec1f80114a88480451cf33017
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-06 15:34:30 +0900

    Load evidence overlap styles before dated portfolio patches

--- portfolio-latest.js
+++ portfolio-latest.js
@@ -1,5 +1,6 @@
 (function(){
   "use strict";
+  document.write('<link rel="stylesheet" href="evidence-partition.css?v=20260906a" />');
   document.write('<script src="portfolio-20260905.js?v=20260905a"><\/script>');
   document.write('<script src="portfolio-20260906.js?v=20260906a"><\/script>');
 })();
