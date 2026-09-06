82d68d0c6b0dc1900cddf04e7e085093803585b4
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-06 21:03:46 +0900

    Load perspective puzzle layer

--- portfolio-latest.js
+++ portfolio-latest.js
@@ -2,9 +2,11 @@
   "use strict";
   document.write('<link rel="stylesheet" href="evidence-partition.css?v=20260906a" />');
   document.write('<link rel="stylesheet" href="core-bridges.css?v=20260906h" />');
+  document.write('<link rel="stylesheet" href="world-game.css?v=20260906a" />');
   document.write('<script src="portfolio-20260905.js?v=20260905a"><\/script>');
   document.write('<script src="portfolio-20260906.js?v=20260906a"><\/script>');
   document.write('<script src="tmop.js?v=20260906b"><\/script>');
   document.write('<script src="theory-reality.js?v=20260906b"><\/script>');
   document.write('<script src="core-bridges.js?v=20260906h"><\/script>');
+  document.write('<script src="world-game.js?v=20260906a"><\/script>');
 })();
