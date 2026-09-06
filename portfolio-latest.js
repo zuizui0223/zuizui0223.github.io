e95bb7c581cd410d305110e5b0c46ee9457e80ba
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-06 20:16:47 +0900

    Load theory-reality interface layer

--- portfolio-latest.js
+++ portfolio-latest.js
@@ -1,9 +1,10 @@
 (function(){
   "use strict";
   document.write('<link rel="stylesheet" href="evidence-partition.css?v=20260906a" />');
-  document.write('<link rel="stylesheet" href="core-bridges.css?v=20260906f" />');
+  document.write('<link rel="stylesheet" href="core-bridges.css?v=20260906g" />');
   document.write('<script src="portfolio-20260905.js?v=20260905a"><\/script>');
   document.write('<script src="portfolio-20260906.js?v=20260906a"><\/script>');
   document.write('<script src="tmop.js?v=20260906b"><\/script>');
-  document.write('<script src="core-bridges.js?v=20260906f"><\/script>');
+  document.write('<script src="theory-reality.js?v=20260906a"><\/script>');
+  document.write('<script src="core-bridges.js?v=20260906g"><\/script>');
 })();
